d46399ca7ccceaadf315de47510efee554755290
sidharthproject <sidharthpatro001@gmail.com>
2024-04-08 21:17:16 +0530

    Sixteen commit

--- backend/middlewares/Error.js
+++ backend/middlewares/Error.js
@@ -1,8 +1,19 @@
 class ErrorHandler extends Error{
-    constructor(message,statuscode=500){
+    constructor(message,statuscode ,errors =[],
+        stack =""){
         super(message)
         this.statuscode = statuscode
-        
+        this.data = null,
+        this.message = message
+        this.success = false
+        this.errors = errors
+
+        if(stack){
+            this.stack = stack
+        }
+        else{
+            Error.captureStackTrace(this,this.constructor)
+        }
     }
 }
 
