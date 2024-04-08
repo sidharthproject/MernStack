14f37206c4aa95a7c552c91078772cb5d89089d2
sidharthproject <sidharthpatro001@gmail.com>
2024-04-08 20:36:03 +0530

    fffteen commit

--- backend/middlewares/Error.js
+++ backend/middlewares/Error.js
@@ -5,15 +5,7 @@ class ErrorHandler extends Error{
         
     }
 }
-   export const Error = (err, req, res, next) => {
-        let statusCode = err.statusCode || 500;
-        let message = err.message || "Internal Server Error";
-        if (process.env.NODE_ENV === "development") {
-            // In development environment, send stack trace along with error message
-            message = err.message;
-        }
-        res.status(statusCode).json({ success: false, error: message });
-    };
+
     
    
 
