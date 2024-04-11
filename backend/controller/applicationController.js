6489d5c7a60680d00336fe69130d7c60e4f85136
sidharthproject <sidharthpatro001@gmail.com>
2024-04-11 18:26:35 +0530

    thirty five

--- backend/controller/applicationController.js
+++ backend/controller/applicationController.js
@@ -61,101 +61,181 @@ export const JobSeekerDeleteApplication = asyncHndler(async(req,res,next)=>{
      })
 })
 
-export const postApplication = asyncHndler(async(req,res,next)=>{
-cloudinary.v2.config(
+// export const postApplication = asyncHndler(async(req,res,next)=>{
+// cloudinary.v2.config(
+//     {
+//         cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+//         api_key:process.env.CLOUDINARY_API_KEY, 
+//         api_secret:process.env.CLOUDINARY_API_SECRET
+//     }
+// )
+
+//     const {role} = req.user
+//     if(role === "Employer"){
+//       return next(
+//          new ErrorHandler("Employer not allowed to access this resource.", 400)
+//        );
+//      }
+//      if(!req.files || Object.keys(req.files).length === 0)  {
+//       return next(
+//          new ErrorHandler("Resumem is required", 400)
+//        );
+//      } 
+
+//      const {resume} = req.files;
+//      const allowedFormats = ["image/png", "image/jpeg", "image/webp"];
+
+//      // Check if the uploaded file is not in the allowed formats
+//      if (!allowedFormats.includes(resume.mimetype)) {
+//       return next(
+//          new ErrorHandler("Please  upload image in jpg ,png format", 400)
+//        );
+//      }
+//      const cloudinaryResponse =await cloudinary.uploader.upload(resume.tempFilePath);
+ 
+//      if(!cloudinaryResponse || cloudinaryResponse.error ){
+//     console.error("Cloudinary Error: ",cloudinaryResponse.error || "Unknown cloudinary error")
+
+//     return next(
+//       new ErrorHandler("Failed upload resume", 400)
+//     );
+//      }
+
+//      const{name,email,coverLetter,phone,address,jobId} = req.body
+
+//      const applicantId = {
+//          user:req.user._id,
+//          role:"JobSeeker"
+//      }
+
+//      if(!jobId){
+//       return next(
+//          new ErrorHandler("Job not found", 400)
+//        );
+//      }
+
+//      const jobDetails = await Job.findById(jobId)
+//      if(!jobDetails){
+//       return next(
+//          new ErrorHandler("Job not found", 400)
+//        );
+//      }
+
+//      const employerId = {
+//         user:jobDetails.postedBy,
+//         role:"Employer"
+//      }
+
+//      if(!name|| !email || !coverLetter || !phone || !address|| !applicantId || !employerId || !resume){
+//       return next(
+//          new ErrorHandler("Please fill all the field ", 400)
+//        );
+//      }
+
+//      const application = await Application.create({
+//         name,
+//         email,
+//         coverLetter,
+//         phone,
+//         address,
+//         applicantId,
+//         employerId,
+//         resume:{
+//             public_id:cloudinaryResponse.public_id,
+//             url:cloudinaryResponse.secure_url
+//         }
+//      })
+//  res.status(200)
+//     .json({
+//         success:true,
+//         message:"Application submitted successfully",
+//         application
+//     })
+// });
+
+export const postApplication = asyncHndler(async (req, res, next) => {
+
+  cloudinary.v2.config(
     {
         cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
         api_key:process.env.CLOUDINARY_API_KEY, 
         api_secret:process.env.CLOUDINARY_API_SECRET
     }
 )
-
-    const {role} = req.user
-    if(role === "Employer"){
-      return next(
-         new ErrorHandler("Employer not allowed to access this resource.", 400)
-       );
-     }
-     if(!req.files || Object.keys(req.files).length === 0)  {
-      return next(
-         new ErrorHandler("Resumem is required", 400)
-       );
-     } 
-
-     const {resume} = req.files;
-     const allowedFormats = ["image/png", "image/jpeg", "image/webp"];
-
-     // Check if the uploaded file is not in the allowed formats
-     if (!allowedFormats.includes(resume.mimetype)) {
-      return next(
-         new ErrorHandler("Please  upload image in jpg ,png format", 400)
-       );
-     }
-
-    // if(allowedFormats.some((field)=> field = resume.mimetype)){
-    //     throw new ErrorHandler("Please upload your resume png,jpg,webp Format",400)
-        
-    // }
-    //  console.log("Uploaded file mimetype:", resume.mimetype);
-
-     const cloudinaryResponse =await cloudinary.uploader.upload(resume.tempFilePath);
-     console.log(cloudinaryResponse);
-     if(!cloudinaryResponse || cloudinaryResponse.error ){
-    console.error("Cloudinary Error: ",cloudinaryResponse.error || "Unknown cloudinary error")
-
+  const { role } = req.user;
+  if (role === "Employer") {
     return next(
-      new ErrorHandler("Failed upload resume", 400)
+      new ErrorHandler("Employer not allowed to access this resource.", 400)
     );
-     }
+  }
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return next(new ErrorHandler("Resume File Required!", 400));
+  }
 
-     const{name,email,coverLetter,phone,address,jobId} = req.body
+  const { resume } = req.files;
+  const allowedFormats = ["image/png", "image/jpeg", "image/webp"];
+  if (!allowedFormats.includes(resume.mimetype)) {
+    return next(
+      new ErrorHandler("Invalid file type. Please upload a PNG file.", 400)
+    );
+  }
+  const cloudinaryResponse = await cloudinary.uploader.upload(
+    resume.tempFilePath
+  );
 
-     const applicantId = {
-         user:req.user._id,
-         role:"JobSeeker"
-     }
+  if (!cloudinaryResponse || cloudinaryResponse.error) {
+    console.error(
+      "Cloudinary Error:",
+      cloudinaryResponse.error || "Unknown Cloudinary error"
+    );
+    return next(new ErrorHandler("Failed to upload Resume to Cloudinary", 500));
+  }
+  const { name, email, coverLetter, phone, address, jobId } = req.body;
+ 
+const applicantId = {
+  user:req.user._id,
+  role:"JobSeeker"
+}
+  if (!jobId) {
+    return next(new ErrorHandler("Job not found!", 404));
+  }
+  const jobDetails = await Job.findById(jobId);
+  if (!jobDetails) {
+    return next(new ErrorHandler("Job not found!", 404));
+  }
 
-     if(!jobId){
-      return next(
-         new ErrorHandler("Job not found", 400)
-       );
-     }
-
-     const jobDetails = await Job.findById(jobId)
-     if(!jobDetails){
-      return next(
-         new ErrorHandler("Job not found", 400)
-       );
-     }
-
-     const employerId = {
-        user:jobDetails.postedBy,
-        role:"Employer"
-     }
-
-     if(!name|| !email || !coverLetter || !phone || !address|| !applicantId || !employerId || !resume){
-      return next(
-         new ErrorHandler("Please fill all the field ", 400)
-       );
-     }
-
-     const application = await Application.create({
-        name,
-        email,
-        coverLetter,
-        phone,
-        address,
-        applicantId,
-        employerId,
-        resume:{
-            public_id:cloudinaryResponse.public_id,
-            url:cloudinaryResponse.secure_url
-        }
-     })
- res.status(200)
-    .json({
-        success:true,
-        message:"Application submitted successfully",
-        application
-    })
+      const employerId = {
+      user:jobDetails.postedBy,
+      role:"Employer"
+}
+  if (
+    !name ||
+    !email ||
+    !coverLetter ||
+    !phone ||
+    !address ||
+    !applicantId ||
+    !employerId ||
+    !resume
+  ) {
+    return next(new ErrorHandler("Please fill all fields.", 400));
+  }
+  const application = await Application.create({
+    name,
+    email,
+    coverLetter,
+    phone,
+    address,
+    applicantID,
+    employerID,
+    resume: {
+      public_id: cloudinaryResponse.public_id,
+      url: cloudinaryResponse.secure_url,
+    },
+  });
+  res.status(200).json({
+    success: true,
+    message: "Application Submitted!",
+    application,
+  });
 });
