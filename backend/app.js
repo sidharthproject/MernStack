6214a1509f93e9f289ba25b92a3e6bc58ebbb4df
sidharthproject <sidharthpatro001@gmail.com>
2024-04-12 20:24:39 +0530

    fifity six

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded(
     {
         extended:true,
-        limit:"16kb"}))
+       }))
 app.use('/api/v1/user',userRouter)
 app.use('/api/v1/application',applicationRouter)
 app.use('/api/v1/job',jobRouter)
