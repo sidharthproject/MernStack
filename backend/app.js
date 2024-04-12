20c52df3deb022a75e7ca4544e06e7ac170523de
sidharthproject <sidharthpatro001@gmail.com>
2024-04-12 20:46:42 +0530

    fifty seven

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(cors({
 app.use(cookieParser());
 app.use(fileUpload({
     useTempFiles:true,
-    tempFileDir:'/temp/'
+    tempFileDir:'/tmp/'
     }))
 app.use(express.json());
 app.use(express.urlencoded(
