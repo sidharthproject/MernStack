0ffc8c34ac64e779f79f83acd551a991d82169e9
sidharthproject <sidharthpatro001@gmail.com>
2024-04-12 10:04:21 +0530

    fourty eighht

--- backend/middlewares/Error.js
+++ backend/middlewares/Error.js
@@ -1,12 +1,12 @@
 class ErrorHandler extends Error{
-    constructor(message,statuscode ,errors =[],
+    constructor(message,statuscode ,
         stack =""){
         super(message)
         this.statuscode = statuscode
 
-        this.message = message
-        this.success = false
-        this.errors = errors
+        
+      
+        
 
         if(stack){
             this.stack = stack
@@ -16,24 +16,24 @@ class ErrorHandler extends Error{
         }
     }
 }  
-// export const Handler = (err, req, res, next) => {
-//     console.error(err.stack); // Log the error stack trace
+export const Handler = (err, req, res, next) => {
+    console.error(err.stack); // Log the error stack trace
     
-//     let statusCode = err.status || 500;
-//     let errorMessage = err.message || "Internal Server Error";
+    let statusCode = err.status || 500;
+    let errorMessage = err.message || "Internal Server Error";
     
-//     if (err.code === 'ERR_NETWORK') {
-//         // Handle network error
-//         statusCode = 500;
-//         errorMessage = "Network Error: Unable to connect to the server. Please try again later.";
-//     } else if (err.response && err.response.data && err.response.data.message) {
-//         // If the error has a response and the response has a message, send that message
-//         errorMessage = err.response.data.message;
-//     }
+    if (err.code === 'ERR_NETWORK') {
+        // Handle network error
+        statusCode = 500;
+        errorMessage = "Network Error: Unable to connect to the server. Please try again later.";
+    } else if (err.response && err.response.data && err.response.data.message) {
+        // If the error has a response and the response has a message, send that message
+        errorMessage = err.response.data.message;
+    }
     
-//     res.status(statusCode).json({
-//         success: false,
-//         error: errorMessage
-//     });
-// }; 
+    res.status(statusCode).json({
+        success: false,
+        error: errorMessage
+    });
+}; 
 export default ErrorHandler;
