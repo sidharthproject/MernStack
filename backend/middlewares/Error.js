01fd01aa3924419da7b23fae06225594da41b245
sidharthproject <sidharthpatro001@gmail.com>
2024-04-12 18:57:37 +0530

    fifty two

--- backend/middlewares/Error.js
+++ backend/middlewares/Error.js
@@ -1,12 +1,12 @@
 class ErrorHandler extends Error{
-    constructor(message,statuscode ,
+    constructor(message,statuscode ,errors =[],
         stack =""){
         super(message)
         this.statuscode = statuscode
-
-        
-      
-        
+        this.data = null,
+        this.message = message
+        this.success = false
+        this.errors = errors
 
         if(stack){
             this.stack = stack
@@ -18,22 +18,27 @@ class ErrorHandler extends Error{
 }  
 export const Handler = (err, req, res, next) => {
     console.error(err.stack); // Log the error stack trace
-    
-    let statusCode = err.status || 500;
-    let errorMessage = err.message || "Internal Server Error";
-    
     if (err.code === 'ERR_NETWORK') {
         // Handle network error
-        statusCode = 500;
-        errorMessage = "Network Error: Unable to connect to the server. Please try again later.";
+        res.status(500).json({
+            success: false,
+            error: 'Network Error: Unable to connect to the server. Please try again later.'
+        });
     } else if (err.response && err.response.data && err.response.data.message) {
         // If the error has a response and the response has a message, send that message
-        errorMessage = err.response.data.message;
+        res.status(err.status || 500).json({
+            success: false,
+            error: err.response.data.message
+        });
+    } else {
+        // Otherwise, send a generic error message
+        res.status(err.status || 500).json({
+            success: false,
+            error: err.message || "Internal Server Error"
+        });
     }
-    
-    res.status(statusCode).json({
-        success: false,
-        error: errorMessage
-    });
-}; 
-export default ErrorHandler;
+};;
+   
+
+
+  export default ErrorHandler
